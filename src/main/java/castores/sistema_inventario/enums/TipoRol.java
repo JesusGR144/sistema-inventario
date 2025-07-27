36903779acc681abbfcfff1af7951eebab0891ae
Jesus GR <jesusgaytanramirez144@gmail.com>
2025-07-27 12:14:33 -0600

    Agrega y refactoriza Usuario y TipoRol: usa enum Rol y booleano estatus en lugar de IDs numéricos

---
+++ src/main/java/castores/sistema_inventario/enums/TipoRol.java
@@ -0,0 +1,6 @@
+package castores.sistema_inventario.enums;
+
+public enum TipoRol {
+    ADMINISTRADOR,
+    ALMACENISTA
+}
---
+++ src/main/java/castores/sistema_inventario/modelo/Usuario.java
@@ -0,0 +1,34 @@
+package castores.sistema_inventario.modelo;
+
+import castores.sistema_inventario.enums.TipoRol;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String nombre;
+
+    @Column(name = "correo", nullable = false, length = 50, unique = true)
+    private String correo;
+
+    @Column(name = "contrasena", nullable = false, length = 25)
+    private String contrasena;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "rol", nullable = false, length = 15)
+    private TipoRol rol;
+
+    @Column(name = "estatus", nullable = false)
+    private Boolean estatus = true;
+}
